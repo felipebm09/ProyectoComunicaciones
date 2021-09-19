025b05bbbc5832c468934b5ee37cb8bcf2ae4247
felipebm09 <31948483+felipebm09@users.noreply.github.com>
2021-09-19 15:54:15 -0600

    Add files via upload
    Preliminary decoder... pending testing

--- decoder.py
+++ decoder.py
@@ -1,25 +1,44 @@
+import os
 sample = '10010001001111100110010000010100001'
 
+# Decode binary to string
 def BinToDecimal(devidedsample):
-    devidedsample_dec = int(devidedsample,2)
+    devidedsample_dec = int(devidedsample, 2)
     decode = chr(devidedsample_dec)
-    return decode 
-    
-# hacer un main que elvalúe el archivo de entrada, divida en grupos de 7 y
-# evalue en la función de bintodecimal, luego guarde el decodificado en una archivo nuevo.
+    return decode
 
-contador = 0
-devidedsample = ''
-decodesample = ''
-for i in sample:
-    devidedsample += i
-    contador += 1
-    if(contador == 7):
-        decodesample += BinToDecimal(devidedsample)
-        contador = 0
-        devidedsample = ''
-        print(decodesample)
+# Reads binary from a file
+def ReadFile(filename):
+    archivo = open(filename, mode='r')
+    texto = archivo.read()
+    archivo.close()
+    # Podemos mostrar en pantalla el archivo que se acaba de cargar
+    print(texto)
+    return texto
+
+# writes decoded message to a file
+def WriteFile(filename, datos):
+    file = open(filename, "w")
+    file.write(datos)
+    file.close()
+
+# Calls the other functions
+def __main__():
+    sample = ReadFile('C:/Users/felip/Documents/2s2021/Ingeniería de comunicaciones/Proyecto/ProyectoComunicaciones/Message.txt')
+    contador = 0
+    devidedsample = ''
+    decodesample = ''
+    for i in sample:
+        devidedsample += i
+        contador += 1
+        if(contador == 7):
+            decodesample += BinToDecimal(devidedsample)
+            contador = 0
+            devidedsample = ''
+    print(decodesample)
+     
+    WriteFile('C:/Users/felip/Documents/2s2021/Ingeniería de comunicaciones/Proyecto/ProyectoComunicaciones/DecodeMessage.txt', decodesample)
 
 
 
-
+__main__()
